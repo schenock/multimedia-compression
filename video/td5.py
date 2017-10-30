d7e91b261f01df0ac940bf2d1b63b197f855349c
schenock <mitrevdane@gmail.com>
2017-10-30 17:29:28 +0100

    Finished TD5.

--- video/td5.py
+++ video/td5.py
@@ -1,6 +1,8 @@
 import matplotlib
 import numpy as np
 
+
+# TODO: Remove comments 1
 def exhaustive_search(reference_frame, current_frame, block_size, padding):
     """
     Performs exhaustive serach, given 2 frames: the current and the reference frame
@@ -8,8 +10,8 @@ def exhaustive_search(reference_frame, current_frame, block_size, padding):
     :param int padding: Search area around the current block - in pixels
 
     """
-    i = 0
-    j = 0
+    row = 0
+    col = 0
     m = 0
     n = 0
 
@@ -18,45 +20,90 @@ def exhaustive_search(reference_frame, current_frame, block_size, padding):
     print "block size: " + str(block_size)
     print "search length: " + str(search_size)
 
-    for i in range(0, len(current_frame) - block_size + 1, block_size):
-        for j in range(0, len(current_frame[0]) - block_size + 1, block_size):
-            bx = i + block_size
-            by = j + block_size
+    for row in range(0, len(current_frame) - block_size + 1, block_size):
+        for col in range(0, len(current_frame[0]) - block_size + 1, block_size):
+            bx = row + block_size
+            by = col + block_size
             print "bx = " + str(bx)
             print "Current Frame: "
-            print current_frame[i:i + block_size, j:j + block_size]
+            print current_frame[row:row + block_size, col:col + block_size]
             print " ----------------------------- "
-            current_block = current_frame[i:bx,j:by]
-            # Perform exhaustive search in search-area defined by P for each block - current_block current_frame[i:i + N, j:j + N]
-            x = i - padding if i - padding >= 0 else 0
-            y = i + block_size + padding if i + block_size + padding >= 0 else 0
-            z = j - padding if j - padding >= 0 else 0
-            w = j + block_size + padding if j + block_size + padding >= 0 else  0
-
-            search_area = reference_frame[x:y, z:w]
-
-            print "Searching for search area:\n"
-            print search_area
-
-            search_for_block(current_block, search_area, block_size)
+            current_block = current_frame[row:bx,col:by]
+            # Call search function here:
+            # current_block, reference_frame, block_size, padding
+            # Perform exhaustive search in search-area defined by P for each block - current_block current_frame[row:row + N, col:col + N]
+            search_for_block(current_block, reference_frame, block_size, padding, row, col)
 
 
-def search_for_block(current_block, search_area, block_size):
+#TODO: Remove prints 2
+def search_for_block(current_block, reference_frame, block_size, padding, row, col):
+
+    # Calculate search area
+    x = row - padding if row - padding >= 0 else 0
+    y = row + block_size + padding if row + block_size + padding >= 0 else 0
+    z = col - padding if col - padding >= 0 else 0
+    w = col + block_size + padding if col + block_size + padding >= 0 else  0
+
+    search_area = reference_frame[x:y, z:w]
+
+    print "Searching for search area:\n"
+    print search_area
 
     print "=================================================================="
-    for i in range(0, len(search_area) - block_size+1, 1):
-        for j in range(0, len(search_area[0]) - block_size+1, 1):
-            search_block = search_area[i:(i + block_size), j:(j + block_size)]
+
+    min_mse = 9999999
+    xAxisCoordinates = 99999999
+    yAxisCoordinates = 99999999
+
+    for search_row in range(0, len(search_area) - block_size + 1, 1):
+        for search_col in range(0, len(search_area[0]) - block_size + 1, 1):
+            print "HH " + str(len(search_area[0]))
+            search_block = search_area[search_row:(search_row + block_size), search_col:(search_col + block_size)]
             print "Current block\n" + str(current_block)
             print "Search block: \n" + str(search_block)
-
+            mse = ((current_block - search_block) ** 2).mean(axis = None)
+            print "MSE: " + str(mse)
             print np.allclose(search_block, current_block)
+            if(mse < min_mse):
+                min_mse = mse
+                # Save coordinates of current block
+                print "%%%%%%%%%%"
+                xAxisCoordinates = col - (search_col + x)
+                yAxisCoordinates = (row - (search_row + z))*-1 # Multiply by -1 because the origin is top left here and bottom left in the plot
+
+    if min_mse == 0:
+        print "MIN MSE: " + str(min_mse)
+        print "RES X: " + str(xAxisCoordinates)
+        print "RES Y: " + str(yAxisCoordinates)
+        print "ROW: " + str(row)
+        print "COL: " + str(col)
 
     print "=================================================================="
 
 
 
-a = np.arange(100).reshape(10,10)
+reference = np.arange(100).reshape(10,10)
 b = np.arange(100).reshape(10,10)
 
-exhaustive_search(a, b, 3, 2)
+current_frame = np.zeros(100).reshape(10, 10)
+current_frame[0, 2] = 5
+current_frame[0, 3] = 5
+current_frame[1, 2] = 5
+current_frame[1, 3] = 5
+
+#reference[2, 1] = 5
+#reference[2, 2] = 5
+#reference[3, 1] = 5
+#reference[3, 2] = 5
+
+reference[0, 2] = 5
+reference[0, 3] = 5
+reference[1, 2] = 5
+reference[1, 3] = 5
+
+
+print current_frame
+print reference
+
+#print a
+exhaustive_search(reference, current_frame, 2, 2)
